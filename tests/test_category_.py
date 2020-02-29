b28caa655e998d55489754ab10f12c6bed3a5e62
Christian Chavez <christian.chavez@bos.no>
2020-02-29 21:43:35 +0100

    test: Suggestion on hwo to minimalize LoC for tests

--- tests/test_category_.py
+++ tests/test_category_.py
@@ -8,47 +8,37 @@ from ammo_finder.scrape.pvas_scraper import PvasScraper
 
 # @pytest.mark.paramerize("data", [ "/produkter/ammunisjon/rifle", "/produkter/ammunisjon/hagle", "/produkter/ammunisjon/haandvaapen", "/produkter/ammunisjon/rimfire"])
 def test_generate_category_rifle():
-    url = "/produkter/ammunisjon/rifle"
-
-    result = Category.extract(url)
-
-    assert result is not None
+    result = Category.extract(
+        "/produkter/ammunisjon/rifle"
+    )
     assert result == Category.RIFLE
 
 
 def test_generate_category_shotgun():
-    url = "/produkter/ammunisjon/hagle"
-
-    result = Category.extract(url)
-
-    assert result is not None
+    result = Category.extract(
+        "/produkter/ammunisjon/hagle"
+    )
     assert result == Category.SHOTGUN
 
 
 def test_generate_category_handgun():
-    url = "/produkter/ammunisjon/haandvaapen"
-
-    result = Category.extract(url)
-
-    assert result is not None
+    result = Category.extract(
+        "/produkter/ammunisjon/haandvaapen"
+    )
     assert result == Category.HANDGUN
 
 
 def test_generate_category_rimfire():
-    url = "/produkter/ammunisjon/rimfire"
-
-    result = Category.extract(url)
-
-    assert result is not None
+    result = Category.extract(
+        "/produkter/ammunisjon/rimfire"
+    )
     assert result == Category.RIMFIRE
 
 
 def test_generate_category_pistol_returns_correct_category():
-    url = "https://www.2alfa.no/ammunisjon/ladet-ammo/pistol.hml"
-
-    result = Category.extract(url)
-
-    assert result is not None
+    result = Category.extract(
+        "https://www.2alfa.no/ammunisjon/ladet-ammo/pistol.hml"
+    )
     assert result == Category.HANDGUN
 
 

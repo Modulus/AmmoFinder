5d738cbfa8f3bc2b0288e45b4019f83fed879d30
Christian Chavez <christian.chavez@bos.no>
2020-02-29 11:07:13 +0100

    fix: Fix tests with new repo package layout

--- tests/test_category_.py
+++ tests/test_category_.py
@@ -1,6 +1,6 @@
-from scrape.pvas_scraper import PvasScraper
+from ammo_finder.scrape.pvas_scraper import PvasScraper
 import pytest
-from core.category import Category
+from ammo_finder.core.category import Category
 
 
 # @pytest.mark.paramerize("data", [ "/produkter/ammunisjon/rifle", "/produkter/ammunisjon/hagle", "/produkter/ammunisjon/haandvaapen", "/produkter/ammunisjon/rimfire"])
@@ -19,7 +19,7 @@ def test_generate_category_shotgun():
 
 
     assert result != None
-    assert result == Category.SHOTGUN        
+    assert result == Category.SHOTGUN
 
 def test_generate_category_shotgun():
     url =  "/produkter/ammunisjon/haandvaapen"
@@ -28,15 +28,15 @@ def test_generate_category_shotgun():
 
 
     assert result != None
-    assert result == Category.HANDGUN            
+    assert result == Category.HANDGUN
 
 def test_generate_category_rimfire():
-    url = "/produkter/ammunisjon/rimfire"    
+    url = "/produkter/ammunisjon/rimfire"
 
     result = Category.extract(url)
 
     assert result != None
-    assert result == Category.RIMFIRE      
+    assert result == Category.RIMFIRE
 
 def test_generate_category_pistol_returns_correct_category():
     url = "https://www.2alfa.no/ammunisjon/ladet-ammo/pistol.hml"
@@ -44,7 +44,7 @@ def test_generate_category_pistol_returns_correct_category():
     result = Category.extract(url)
 
     assert result != None
-    assert result == Category.HANDGUN 
+    assert result == Category.HANDGUN
 
 def test_genrate_category_has_incorrect_value_raises_value_error():
     with pytest.raises(ValueError):
